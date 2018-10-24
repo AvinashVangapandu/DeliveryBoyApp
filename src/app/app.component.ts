2d46be13d0d6dd6ff37f36eacfeb027ba0c081b6
Avinash <avinash.vangapandu@siemens.com>
2018-10-24 15:52:23 +0530

    code changes includes , background mode and firebase Db insertion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,25 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
-import { TabsPage } from '../pages/tabs/tabs';
-
+import * as firebase from 'firebase';
+import { TrackingPage } from '../pages/tracking/tracking';
+import { HomePage } from '../pages/home/home';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabase, AngularFireDatabaseModule } from 'angularfire2/database';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  rootPage: any = TrackingPage;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, AFD: AngularFireDatabase) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      AFD.list('/DeliveryBoyLocation/').set('sexy', 'shiva');
+
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,39 +2,51 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
+import * as firebase from 'firebase';
+import { TrackingPage } from '../pages/tracking/tracking';
 import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
-
+import { LocationTracker } from '../providers/location-tracker';
+import { AngularFireModule } from 'angularfire2';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AngularFireDatabase, AngularFireDatabaseModule } from 'angularfire2/database';
 
+import { BackgroundMode } from '@ionic-native/background-mode';
+
+import { BackgroundGeolocation } from '@ionic-native/background-geolocation';
+import { Geolocation } from '@ionic-native/geolocation';
+var config = {
+  apiKey: "AIzaSyA18JPg3AKvecrd7_JFQfNinUc40e9huyQ",
+  authDomain: "gmprod-2018.firebaseapp.com",
+  databaseURL: "https://gmprod-2018.firebaseio.com",
+  projectId: "gmprod-2018",
+  storageBucket: "gmprod-2018.appspot.com",
+  messagingSenderId: "303861404647"
+};
+firebase.initializeApp(config);
 @NgModule({
   declarations: [
-    MyApp,
-    AboutPage,
-    ContactPage,
+    MyApp, TrackingPage,
+
     HomePage,
-    TabsPage
+
   ],
   imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
+    BrowserModule, AngularFireDatabaseModule, AngularFireModule.initializeApp(config),
+    IonicModule.forRoot(MyApp),
+
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    AboutPage,
-    ContactPage,
+    MyApp, TrackingPage,
+
     HomePage,
-    TabsPage
+
   ],
-  providers: [
-    StatusBar,
+  providers: [BackgroundMode, BackgroundGeolocation,
+    StatusBar, LocationTracker, Geolocation,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
-export class AppModule {}
+export class AppModule { }
